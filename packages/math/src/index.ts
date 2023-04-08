fa5fff2d7abf75146595b7651ef5b4ece1ca64cb
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-08 11:55:42 +0200

    build: update export in index

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -1,8 +1,14 @@
-import getRandomInRange from './getRandomInRange';
-import degrees from './degrees';
-import radian from './radian';
-import even from './even';
-import odd from './odd';
+import degreesModule from './degrees';
+import evenModule from './even';
+import getRandomInRangeModule from './getRandomInRange';
+import oddModule from './odd';
+import radianModule from './radian';
+
+export const degrees = degreesModule;
+export const even = evenModule;
+export const getRandomInRange = getRandomInRangeModule;
+export const odd = oddModule;
+export const radian = radianModule;
 
 export default {
     degrees,
