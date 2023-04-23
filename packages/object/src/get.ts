20b0ba6c853de42e1f9217c1a687411a593fb051
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-23 13:51:31 +0200

    docs: add docs system

--- packages/object/src/get.ts
+++ packages/object/src/get.ts
@@ -1,4 +1,11 @@
-const get = <T>(object: any, path: string, defaultValue: T): T => {
+/**
+ *
+ * @param object - Origin object who get is perform
+ * @param path - Path to get in object
+ * @param defaultValue - Fallback value if the value associated to the path is not set in object
+ * @since 0.0.1
+ */
+const get = <T>(object: any, path: string, defaultValue?: T ): T | undefined => {
     const value: T | undefined = object[path];
     if (value) {
         return value as T;
