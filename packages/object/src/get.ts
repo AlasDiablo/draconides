55a79632ba58d3a330c1df3c8a9f238569c7c50a
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-27 19:04:35 +0200

    docs: update docs system

--- packages/object/src/get.ts
+++ packages/object/src/get.ts
@@ -1,8 +1,23 @@
 /**
- *
+ * Get the first level element of an object or return default value
  * @param object - Origin object who get is perform
  * @param path - Path to get in object
  * @param defaultValue - Fallback value if the value associated to the path is not set in object
+ * @returns - Return the value corresponding the path or undefined or default value if none found
+ * @example
+ * ```
+ * // Create an object
+ * const myObject = {
+ *     value: 'test',
+ * };
+ *
+ * // Return 'default'
+ * get(myObject, 'key', 'default');
+ *
+ * // Return 'test'
+ * get(myObject, 'value', 'default');
+ * ```
+ * @version 0.0.4
  * @since 0.0.1
  */
 const get = <T>(object: any, path: string, defaultValue?: T ): T | undefined => {
--- packages/object/src/set.ts
+++ packages/object/src/set.ts
@@ -1,3 +1,23 @@
+/**
+ * Replace or set an element in the first level of an object
+ * @param object - Origin object who set is perform
+ * @param path - Path to set in object
+ * @param value - Value to set to the associated path in the object
+ * @returns - Return previous value or undefined
+ * @example
+ * ```
+ * // Create an empty object
+ * const myObject = {};
+ *
+ * // Return undefined
+ * set(myObject, 'key', 'value');
+ *
+ * // Print {key: 'value'}
+ * console.log(myObject);
+ * ```
+ * @version 0.0.2
+ * @since 0.0.1
+ */
 const set = <T>(object: any, path: string, value: T): T | undefined => {
     const previousValue: T | undefined = object[path];
     object[path] = value;
