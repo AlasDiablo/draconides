07900ea7c066dce214d23825e3aac4c9a7393079
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-29 18:48:39 +0200

    feat: add eslint config

---
+++ packages/eslint-config-base/README.md
@@ -0,0 +1,19 @@
+![npm](https://img.shields.io/npm/dw/@draconides/eslint-config-base)
+![npm](https://img.shields.io/npm/dt/@draconides/eslint-config-base)
+![GitHub issues by-label](https://img.shields.io/github/issues/AlasDiablo/draconides/@draconides/eslint-config-base)
+![NPM](https://img.shields.io/npm/l/@draconides/eslint-config-base?color=%234c1)
+![npm (scoped)](https://img.shields.io/npm/v/@draconides/eslint-config-base)
+
+# Draconides
+
+Draconides (French word of [Draconids](https://en.wikipedia.org/wiki/Draconids)) is a monorepo library make by AlasDiablo to add useful function, utils and tools.
+
+## Draconides eslint-config-base
+
+This module of Draconides is a configuration for eslint
+
+## Installation
+
+```bash
+npm install --save @draconides/eslint-config-base
+```
---
+++ packages/eslint-config-base/index.js
@@ -0,0 +1,75 @@
+module.exports = {
+    plugins: [
+        'eslint-plugin-import',
+    ],
+    rules: {
+        'no-console': 'warn',
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'no-unused-vars': [
+            'error',
+            {
+                'vars': 'all',
+                'args': 'after-used',
+                'ignoreRestSiblings': false
+            }
+        ],
+        'no-import-assign': 'error',
+        'no-undef': [
+            'error',
+            {
+                'typeof': true
+            }
+        ],
+        'no-unexpected-multiline': 'error',
+        'no-self-assign': 'error',
+        'no-unreachable': 'error',
+        'no-unreachable-loop': 'error',
+        'no-unsafe-optional-chaining': 'error',
+        'no-use-before-define': 'error',
+        'no-useless-backreference': 'error',
+        'valid-typeof': [
+            'error',
+            {
+                'requireStringLiterals': false
+            }
+        ],
+        'camelcase': 'error',
+        'curly': 'error',
+        'eqeqeq': 'error',
+        'no-shadow': 'error',
+        'no-var': 'error',
+        'radix': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-namespace': 'error',
+        'import/no-empty-named-blocks': 'error',
+        'import/no-useless-path-segments': 'error',
+        'import/consistent-type-specifier-style': 'error',
+        'import/order': [
+            'error',
+            {
+                'groups': [
+                    'index',
+                    'sibling',
+                    'parent',
+                    'internal',
+                    'external',
+                    'builtin',
+                    'object',
+                    'type'
+                ],
+                'alphabetize': {
+                    'order': 'asc',
+                    'caseInsensitive': true
+                },
+                'newlines-between': 'never'
+            }
+        ],
+        'indent': ['error', 4],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+    }
+};
