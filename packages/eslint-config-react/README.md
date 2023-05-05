4f680ecdf9b531b477ed8d7ce00eb5f8fce642dc
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-05-05 15:20:09 +0200

    feat: add react eslint config

---
+++ packages/eslint-config-react/README.md
@@ -0,0 +1,21 @@
+![npm](https://img.shields.io/npm/dw/@draconides/eslint-config-react)
+![npm](https://img.shields.io/npm/dt/@draconides/eslint-config-react)
+![GitHub issues by-label](https://img.shields.io/github/issues/AlasDiablo/draconides/@draconides/eslint-config-react)
+![NPM](https://img.shields.io/npm/l/@draconides/eslint-config-react?color=%234c1)
+![npm (scoped)](https://img.shields.io/npm/v/@draconides/eslint-config-react)
+
+# Draconides
+
+Draconides (French word of [Draconids](https://en.wikipedia.org/wiki/Draconids))
+is a monorepo library made by AlasDiablo to add useful function,
+utils, and tools.
+
+## Draconides eslint-config-react
+
+This module of Draconides is a configuration for ESLint for React
+
+## Installation
+
+```bash
+npm install --save-dev @draconides/eslint-config-react
+```
---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+    ],
+    rules: {
+        'react/destructuring-assignment': ['error', 'always'],
+        'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
+        'react/hook-use-state': ['error', { allowDestructuredState: true }],
+        'react/jsx-boolean-value': ['error', 'always'],
+        'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
+        'react/jsx-closing-tag-location': 'error',
+        'react/jsx-equals-spacing': ['error', 'never'],
+        'react/jsx-fragments': ['error', 'syntax'],
+        'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+        'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
+        'react/jsx-no-target-blank': [
+            'error',
+            {
+                enforceDynamicLinks: 'always',
+                warnOnSpreadAttributes: true,
+            },
+        ],
+        'react/jsx-no-useless-fragment': 'error',
+        'react/no-array-index-key': 'error',
+        'react/no-deprecated': 'error',
+        // Material UI import rule
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: ['@mui/*', '@mui/*/*/*'],
+            },
+        ],
+    },
+};
