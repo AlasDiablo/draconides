84184a5678ce3804e27e9971338f6ca1366c1e57
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-05-05 15:34:54 +0200

    fix: remove peer dep and eslint/recommended from React plugin

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     extends: [
-        'eslint:recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
